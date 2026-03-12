ad383862553d22d347d1c1c88c4131b31646e342
ahashmi5401 <ahashmi5401@gmail.com>
2026-03-12 16:58:29 +0500

    variable in c++

---
+++ 01-intro-cpp/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+string car = "Toyota"; //global variable
+main () {
+    cout << "================> Enter Your Name <================ \n";
+    string name ; //local variable
+    cin >> name;
+    cout << name;
+    return 0;
+}
