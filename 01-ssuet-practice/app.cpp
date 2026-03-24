2895d4f709e247946227e8409f7b2e91d20aa75e
ahashmi5401 <ahashmi5401@gmail.com>
2026-03-24 14:25:11 +0500

    ssuet practice

---
+++ 01-ssuet-practice/app.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\a"; 
+
+    cout << "\nSSUET Welcome Screen\n";
+    cout << "--------------------------\n";
+
+
+    cout << "Name:\t\tMuhammad Ayan Hashmi" << endl;
+    cout << "Department:\tComputer Engineering" << endl;
+
+    cout << "Message:\t\"The only way to learn a new programming language is by writing programs in it.\"" << endl;
+
+
+    cout << "Path: K:\\Cpp-playground" << endl;
+
+
+    cout << "Current Grade:\t'A'" << endl;
+
+
+    cout << "Loading System... Please Wait";
+    cout << "\rSystem Ready!                " << endl;
+
+    cout << "Initial Score: 100\b5" << endl;
+
+    return 0;
+}
